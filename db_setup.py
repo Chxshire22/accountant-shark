e0d45092fc470a68773c5bdf69af01797750d965
alice <alice@arch-server>
2025-07-06 00:15:51 +0800

    initial commit, POC for /register done

---
+++ db_setup.py
@@ -0,0 +1,25 @@
+import sqlite3
+
+connection = sqlite3.connect("tele_shark.db")
+cursor = connection.cursor()
+
+drop_user_table = "DROP TABLE IF EXISTS Users"
+drop_group_table = "DROP TABLE IF EXISTS Groups"
+drop_debt_table = "DROP TABLE IF EXISTS Debts"
+drop_user_groups_table = "DROP TABLE IF EXISTS User_Groups"
+
+create_user_table = """CREATE TABLE IF NOT EXISTS Users(user_id INTEGER PRIMARY KEY, username TEXT)"""
+
+create_group_table = """CREATE TABLE IF NOT EXISTS Groups(group_id INTEGER PRIMARY KEY)"""
+
+create_debt_table = """CREATE TABLE IF NOT EXISTS Debts(debt_id INTEGER PRIMARY KEY,debtee_id INTEGER,debtor_id INTEGER,amount INTEGER,FOREIGN KEY (debtee_id) REFERENCES Users (user_id),FOREIGN KEY (debtor_id) REFERENCES Users (user_id))"""
+
+create_user_groups_table = """CREATE TABLE IF NOT EXISTS
+User_Groups(user_id INTEGER,group_id INTEGER)"""
+
+commands_list = [drop_user_table, drop_group_table, drop_debt_table, drop_user_groups_table, create_user_table,create_group_table, create_user_groups_table, create_debt_table]
+
+print(commands_list)
+
+for command in commands_list:
+    cursor.execute(command)
---
+++ main.py
@@ -0,0 +1,171 @@
+import sqlite3
+from typing import Final
+from telegram import Chat, ChatMember, ChatMemberUpdated, Update
+from telegram.ext import (
+    Application,
+    ApplicationBuilder,
+    CommandHandler,
+    ContextTypes,
+    MessageHandler,
+    filters,
+)
+
+# TELEGRAM
+TOKEN: Final = "7691516733:AAF8xN3UuK55jte-Yv1mbRilgxzUwNcd9rE"
+BOT_USERNAME: Final = "@AccountantSharkbot"
+
+# DB Connection
+connection = sqlite3.connect("tele_shark.db")
+cursor = connection.cursor()
+
+async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "I start automatically, but make sure that everyone participating registers with '/register'\nUse '/help' to learn how to use me."
+    )
+
+
+async def register_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    chatID: int = update.effective_chat.id
+    currentUsername: str = update.effective_user.username
+    currentUserID: int = update.effective_user.id
+    user_search = cursor.execute(f"SELECT * FROM Users WHERE user_id={currentUserID};")
+    if user_search.fetchone() is None:
+        cursor.execute(f"INSERT INTO Users (user_id, username) VALUES ({currentUserID},'{currentUsername}');")
+
+    chat_search = cursor.execute(f"SELECT * FROM Groups WHERE group_id={chatID};")
+    if chat_search.fetchone() is None:
+        cursor.execute(f"INSERT INTO Groups (group_id) VALUES ({chatID});")
+        
+    user_groups_search = cursor.execute(f"SELECT * FROM User_Groups WHERE user_id={currentUserID} AND group_id={chatID};")
+    if user_groups_search.fetchone() is None:
+        cursor.execute(f"INSERT INTO User_Groups (user_id,group_id) VALUES ({currentUserID}, {chatID});")
+        await update.message.reply_text(f"@{currentUsername}'s record for this group is created.")
+    else:
+        await update.message.reply_text("Your record for this chat is already created, you don't have to do that.")
+
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        """
+What do I do?  I record payments made by users, for their friends in the group chat.
+
+Paid for your friend/s? - 
+Send me the information in this format: 'I paid $89 for @user1 @user2 @user3.'
+This will record the amount spent, and split it between the different users.
+
+Got paid what you're owed? -
+Send me this, "@user paid me $X" 
+I'll check if they exist, owe you that much or more, then deduct it from the balance.
+The debt is cleared once the debt is 0. 
+
+Not sure how much you owe? -
+Send me this, "How much do I owe?"
+I'll tell you who you owe and how much.
+
+Not sure who owes you? -
+Send me this, "Who owes me?"
+I'll tell you who owes you and how much. 
+        """
+    )
+
+
+# Responses
+
+
+# need to pass groupchat-id, current user-id,
+def handle_response(text: str, chatID, currentUsername, currentUserID):
+    processed: str = text.lower()
+    if "hello" in processed:
+        hello_str = (
+            f"chat ID: {chatID}\nUsername: @{currentUsername}\nUserID: {currentUserID}"
+        )
+        return hello_str
+    if "i paid" in processed:
+        return "your payment was recorded."
+    if "paid me" in processed:
+        return "okay, debt cleared"
+    if "how much do i owe" in processed:
+        return "you owe ..."
+    if "owes me" in processed:
+        return "this user owes you"
+    return "I don't understand you, send 'help' for instructions on how to interact with me"
+
+
+# messages should be parsed in handle_mesage function
+# the functions should produce their own strings as a feedback, incl errors to users. 
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    message_type: str = update.message.chat.type
+    text: str = update.message.text
+    chatID: int = update.effective_chat.id
+    currentUsername: str = update.effective_user.username
+    currentUserID: int = update.effective_user.id
+
+    print(f'User ({update.message.chat.id}) in {message_type}: "{text}"')
+    print(f"{currentUsername}")
+
+    if message_type == "group":
+        if BOT_USERNAME in text:
+            new_text: str = text.replace(BOT_USERNAME, "").strip()
+            # parse the message text 
+            # once message text is parsed, find out whether to pass info into payment creation, debt payment, table lookup for debts owing or debts owed. 
+            response: str = handle_response(
+                new_text, chatID, currentUsername, currentUserID
+            )
+        else:
+            return
+    else:
+        response: str = handle_response(text, chatID, currentUsername, currentUserID)
+
+    print("BOT:", response)
+    await update.message.reply_text(response)
+
+
+# Server side errors printing.
+async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print(f"Update {update} caused error {context.error}")
+
+
+if __name__ == "__main__":
+    app = ApplicationBuilder().token(TOKEN).build()
+
+    # COMMANDS forward slashes for specific commands
+    app.add_handler(CommandHandler("start", start_command))
+    app.add_handler(CommandHandler("help", help_command))
+    app.add_handler(CommandHandler("register", register_command))
+
+    # MESSAGES
+    app.add_handler(MessageHandler(filters.TEXT, handle_message))
+
+    # ERRORS
+    app.add_error_handler(error)
+
+    # POLLING
+    print("polling.")
+    app.run_polling(poll_interval=3)
+
+
+# Business logic for payment records.
+def paid_for_group():
+    return
+
+
+def received_payment():
+    return
+
+
+def debts():
+    return
+
+
+def owed():
+    return
+
+
+# <leader>xx shows all the linting issues...
+#TODO: create function to register users into the db.
+#TODO: create function to register group into the db
+#TODO: create function to POST new debt
+#TODO: create function to PUT/UPDATE debt record of one user against another. 
+#TODO: create function to GET debts owed to user.
+#TODO: create function to GET debts owed to others.
+
